fbfccbd0e3a9b35302ddb127009337f5e1d9d2ec
leonhaft <leonhaft@outlook.com>
2014-09-16 14:04:31 +0800

    init

---
+++ CanvasApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CanvasApp.MvcApplication" Language="C#" %>
---
+++ CanvasApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CanvasApp.Startup))]
+namespace CanvasApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
